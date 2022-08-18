76128e1f3ebeaaef18f7dc534f84a94c504fb8fd
yogesh <yogeshannex360@gmail.com>
2022-08-18 16:37:42 +0530

    yogesh

--- hiremestore/core/models.py
+++ hiremestore/core/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
-
+from PIL import Image
 
 # Create your models here.
+
 
 
 class website_profile(models.Model):
@@ -23,6 +24,7 @@ class website_profile(models.Model):
     def __str__(self):
         return self.website_title
 
+
 class Category(models.Model):
     image = models.ImageField(upload_to='img', max_length=250)
     name = models.CharField(max_length=30)
@@ -31,9 +33,9 @@ class Category(models.Model):
     feature = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.name
+
 
 class SubCategory(models.Model):
     cat_id = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -43,9 +45,9 @@ class SubCategory(models.Model):
     feature = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.name
+
 
 class Testimonails(models.Model):
     profile = models.ImageField(upload_to='img', max_length=250)
@@ -53,6 +55,42 @@ class Testimonails(models.Model):
     about = models.CharField(max_length=50)
     review = models.CharField(max_length=200)
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.profile.path)
+
+        if img.height > 100 or img.weight > 100:
+            output_size = (100, 100)
+            img.thumbnail(output_size)
+            img.save(self.profile.path)
+
+    def __str__(self):
+        return self.name
+
+
+class About(models.Model):
+    main_img = models.ImageField(upload_to='img', max_length=250)
+    description = models.TextField()
+    member_name = models.CharField(max_length=50)
+    member_img = models.ImageField(upload_to='img', max_length=250)
+    member_skill = models.CharField(max_length=30)
+    fb_link = models.CharField(max_length=75)
+    twitter_link = models.CharField(max_length=75)
+    insta_link = models.CharField(max_length=75)
+    email_link = models.CharField(max_length=75)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.member_name
+
+
+class Contact(models.Model):
+
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    subject = models.CharField(max_length=75)
+    mobile = models.IntegerField()
+    message = models.TextField()
 
     def __str__(self):
         return self.name
