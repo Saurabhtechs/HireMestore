eaddc8013db6f4229c85706fe607cf4603bbf7fc
techheat <iamsoravh380@gmail.com>
2022-08-19 13:42:07 +0530

    commit

--- hiremestore/hiremestore/urls.py
+++ hiremestore/hiremestore/urls.py
@@ -21,13 +21,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('main/login/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
     path('', include('core.urls')),
     path('admins/', include('home.urls')),
-    path('admin/', admin.site.urls)
-
-
+    
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
